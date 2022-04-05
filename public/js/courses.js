be14482e0907098906c2bb008b8271749cdf9d8a
Helena Spencer <helenaspencer@lenamacbook.wifi.local.cmu.edu>
2022-04-04 21:14:01 -0400

    All functionalities working

--- public/js/courses.js
+++ public/js/courses.js
@@ -3,109 +3,165 @@ var allCourses = [
     {courseNum: "57-132", 
         title: "Computer Programming", 
         professor: "Prof Q", 
-        sections: ["1A", "1B", "2A", "2B", "3C"], 
-        units: 10, 
+        sections: ["1AC", "1AE", "1BD", "2AB", "2BC", "3CD"], 
+        units: 10,
         fce: 6, 
-        description: "A course committed to helping students learn the fundamentals of computer programming."},
+        description: "A course committed to helping students learn the fundamentals of computer programming."
+    },
     {courseNum: "67-443", 
         title: "Mobile App Development", 
         professor: "Prof H", 
-        sections: ["A", "B", "C", "D", "E"], 
+        sections: ["1BC", "1DE", "2CD", "2CE", "3AD", "3AE"], 
         units: 12, 
         fce: 16, 
-        description: "Provide students with the knowledge to be able to design and develop their own mobile application"},
+        description: "Provide students with the knowledge to be able to design and develop their own mobile application"
+    },
     {courseNum: "67-373", 
         title: "IS Consulting", 
         professor: "Prof Mertz", 
-        sections: ["1A", "2A", "2B", "3C", "3D", "4E"], 
+        sections: ["1AB", "2AC", "2BC", "3AB", "3CD", "3DE", "4AE"], 
         units: 12, 
         fce: 10, 
-        description: "Students will work with a community partner to help them solve their pain points in an effective and sustainable solution."},
+        description: "Students will work with a community partner to help them solve their pain points in an effective and sustainable solution."
+    },
     {courseNum: "05-535", 
         title: "Applied Machine Learning", 
         professor: "Prof Rose", 
-        sections: ["A", "B", "C"], 
+        sections: ["4AB", "4BE", "4CD"], 
         units: 9, 
         fce: 12, 
-        description: "Learn the basics of machine learning through this low-tech course."},
+        description: "Learn the basics of machine learning through this low-tech course."
+    },
     {courseNum: "16-264", 
         title: "Humanoids", 
         professor: "Prof Atkeson", 
-        sections: ["A", "B"], 
+        sections: ["2AD", "4BD"], 
         units: 12, 
         fce: 6, 
-        description: "Explore the current status of robotics within our society and the potentials of how advance machines can get."},
+        description: "Explore the current status of robotics within our society and the potentials of how advance machines can get."
+    },
     {courseNum: "05-430", 
         title: "Programming Usable Interfaces", 
         professor: "Prof Hudson", 
-        sections: ["1A", "1B", "1C", "2A", "2B"], 
+        sections: ["1AE", "1BD", "1CE", "2AB", "2BC"], 
         units: 15, 
         fce: 20, 
-        description: "Dive into the fundamentals of design and technology within our current society."},
+        description: "Dive into the fundamentals of design and technology within our current society."
+    },
     {courseNum: "05-435", 
         title: "Applied Fabrication in HCI", 
         professor: "Prof Alex", 
-        sections: ["A", "B"], 
+        sections: ["1AB", "2BD"], 
         units: 12, 
         fce: 18, 
-        description: "Experiment with different fabrication methods such as 3D printing and laser cutting to modify materials."},
+        description: "Experiment with different fabrication methods such as 3D printing and laser cutting to modify materials."
+    },
     {courseNum: "17-214", 
         title: "Fundametals of SWE", 
         professor: "Prof Bosch", 
-        sections: ["1A", "1B", "1C", "2A", "2B"],
+        sections: ["1AE", "1BD", "1CE", "2AB", "2BC"],
         units: 12, 
         fce: 16, 
-        description: "An introduction to software engineering for students to learn and apply to future courses."},
+        description: "An introduction to software engineering for students to learn and apply to future courses."
+    },
     {courseNum: "17-330", 
         title: "Object Oriented Programming", 
         professor: "Prof L", 
-        sections: ["1A", "1B", "1C", "2A", "2B"], 
+        sections: ["1AE", "1BD", "1CE", "2AB", "2BC"], 
         units: 12, 
         fce: 9, 
-        description: "A deeper dive into object-oriented programming and various application in Java."},
+        description: "A deeper dive into object-oriented programming and various application in Java."
+    },
     {courseNum: "67-272", 
         title: "Database Design & Dev", 
         professor: "Prof Raja", 
-        sections: ["1A", "1B", "1C", "2A", "2B"], 
+        sections: ["1AC", "1BE", "1DE", "2AC", "2CE"], 
         units: 12, 
         fce: 10, 
-        description: "Familiarize students with databases and the various languages used to add and parse information."},
+        description: "Familiarize students with databases and the various languages used to add and parse information."
+    },
 ];
 var currentCourses = [
     {courseNum: "57-132", 
         title: "Computer Programming", 
         professor: "Prof Q", 
-        section: "1A", 
+        section: "1AE", 
         units: 10, 
         fce: 6, 
         description: "A course committed to helping students learn the fundamentals of computer programming.", 
-        fulfilled: "Information Systems - Tech Core",
-        dayOfWeek: "Monday",
-        startTime: "8:30",
-        endTime: "10:15",
-        index: [{row: 1, col: 0}, {row: 1, col: 4}]},
+        fulfilled: "Information Systems - Tech Core"
+    },
     {courseNum: "67-443", 
         title: "Mobile App Development", 
         professor: "Prof H", 
-        section: "C", 
+        section: "2CD", 
         units: 12, 
         fce: 16, 
         description: "Provide students with the knowledge to be able to design and develop their own mobile application", 
-        fulfilled: "Information Systems - Disciplinary Core",
-        dayOfWeek: "Tuesday",
-        startTime: "11:00",
-        endTime: "12:45",
-        index: [{row: 2, col: 1}, {row: 3, col: 3}]},
+        fulfilled: "Information Systems - Disciplinary Core"
+    },
     {courseNum: "67-373", 
         title: "IS Consulting", 
         professor: "Prof Mertz", 
-        section: "2D", 
+        section: "3AB", 
         units: 12, 
         fce: 10, 
         description: "Students will work with a community partner to help them solve their pain points in an effective and sustainable solution.", 
-        fulfilled: "Information Systems - Professional Core",
-        dayOfWeek: "Thursday",
+        fulfilled: "Information Systems - Professional Core"
+    }
+];
+
+var sectionNums = [
+    {
+        num: "1",
+        startTime: "8:00",
+        endTime: "10:00",
+        rowIndex: 1
+    }, 
+    {
+        num: "2",
+        startTime: "10:30",
+        endTime: "12:30",
+        rowIndex: 2
+    }, 
+    {
+        num: "3",
         startTime: "14:30",
         endTime: "16:30",
-        index: [{row: 2, col: 4}, {row: 4, col: 2}]}
+        rowIndex: 3
+    }, 
+    {
+        num: "4",
+        startTime: "18:00",
+        endTime: "20:00",
+        rowIndex: 4
+    }
+];
+
+var sectionLetters = [
+    {
+        num: "A",
+        dayOfWeek: "Monday",
+        colIndex: 0
+    }, 
+    {
+        num: "B",
+        dayOfWeek: "Tuesday",
+        colIndex: 1
+    }, 
+    {
+        num: "C",
+        dayOfWeek: "Wednesday",
+        colIndex: 2
+    }, 
+    {
+        num: "D",
+        dayOfWeek: "Thursday",
+        colIndex: 3
+    }, 
+    {
+        num: "E",
+        dayOfWeek: "Friday",
+        colIndex: 4
+    }
 ];
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,10 @@
+//Global Vars
+const class_dict = [];
+const storedCourses = JSON.parse(localStorage.getItem('class_list'));
+const class_list = storedCourses ? storedCourses : ['57-132', '67-443', '67-373'];
+const storedSects = JSON.parse(localStorage.getItem('classSection_list'));
+const classSection_list = storedSects ? storedSects : [ {courseNum: "57-132", section: "1AE"}, {courseNum: "67-443", section: "2CD"}, {courseNum: "67-373", section: "3AB"}];
+
 // Removing Popup
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -45,86 +52,70 @@ window.onclick = function(event) {
 }
 
 // Switching sections
-function switchSection(courseNum, courseName){
-    var table = document.querySelector('#schedule');
-
-    updateSection();
-    table.rows[newIndexes[0].row].cells[newIndexes[0].col].classList.add(courseNum);
-    table.rows[newIndexes[1].row].cells[newIndexes[1].col].classList.add(courseNum);
-    table.rows[newIndexes[0].row].cells[newIndexes[0].col].setAttribute('id', 'selected');
-    table.rows[newIndexes[1].row].cells[newIndexes[1].col].setAttribute('id', 'selected');
-    table.rows[newIndexes[0].row].cells[newIndexes[0].col].textContent = courseName;
-    table.rows[newIndexes[1].row].cells[newIndexes[1].col].textContent = courseName;
-    table.rows[oldIndexes[0].row].cells[oldIndexes[0].col].classList.remove(courseNum);
-    table.rows[oldIndexes[1].row].cells[oldIndexes[1].col].classList.remove(courseNum);
-    table.rows[oldIndexes[0].row].cells[oldIndexes[0].col].removeAttribute('id');
-    table.rows[oldIndexes[1].row].cells[oldIndexes[1].col].removeAttribute('id');
-    table.rows[oldIndexes[0].row].cells[oldIndexes[0].col].textContent = "";
-    table.rows[oldIndexes[1].row].cells[oldIndexes[1].col].textContent = "";
+function switchSection(courseNum){
+    console.log(courseNum);
+    updateSection(courseNum);
+    window.location.reload();
     closeModal();
 }
 
-function updateSection() {
+function updateSection(courseNum) {
     var ele = document.getElementsByName('sections');
-      
-    oldIndexes = newIndexes;
+    var classSections = JSON.parse(localStorage.getItem('classSection_list'));
+    var courseSection = classSections.find(e => e.courseNum === courseNum);
+    var section = courseSection.section;
 
     for(i = 0; i < ele.length; i++) {
         if(ele[i].checked){
-        document.getElementById("selection").innerHTML
-                = "Section: "+ele[i].value;
+          section = ele[i].value;
         }
     }
 
-    if(document.getElementById("selection").innerHTML == "Section: A"){
-        newIndexes = [{row: 1, col: 1}, {row: 2, col: 3}];
-    }else if(document.getElementById("selection").innerHTML == "Section: B"){
-        newIndexes = [{row: 2, col: 0}, {row: 3, col: 2}];
-    }else{
-        newIndexes = [{row: 4, col: 3}, {row: 1, col: 4}];
+    //Updating local storage
+    var newClassSections = [];
+    for(let i = 0; i < classSections.length; i++){
+      if(classSections[i].courseNum === courseNum){
+        var s = section;
+      }else{
+        var s = classSections[i].section;
+      }
+      let classSect = {"courseNum": classSections[i].courseNum, "section": s};
+      newClassSections.push(classSect);
     }
+
+    console.log(newClassSections);
+    localStorage.setItem('classSection_list', JSON.stringify(newClassSections));
 }
 
 function removeSection(courseNum) {
-    var table = document.querySelector('#schedule');
+    var classSections = JSON.parse(localStorage.getItem('classSection_list'));
+    var classNums = [];
 
-    table.rows[newIndexes[0].row].cells[newIndexes[0].col].classList.remove(courseNum);
-    table.rows[newIndexes[1].row].cells[newIndexes[1].col].classList.remove(courseNum);
-    table.rows[newIndexes[0].row].cells[newIndexes[0].col].removeAttribute('id');
-    table.rows[newIndexes[1].row].cells[newIndexes[1].col].removeAttribute('id');
-    table.rows[newIndexes[0].row].cells[newIndexes[0].col].textContent = "";
-    table.rows[newIndexes[1].row].cells[newIndexes[1].col].textContent = "";
-    document.getElementById("selection").innerHTML = "Section: ";
+    var newClassSections = [];
+    for(let i = 0; i < classSections.length; i++){
+      if(classSections[i].courseNum === courseNum){
+        continue;
+      }else{
+        classNums.push(classSections[i].courseNum);
+        let classSect = {"courseNum": classSections[i].courseNum, "section": classSections[i].section};
+        newClassSections.push(classSect);
+      }
+    }
+
+    console.log(classNums);
+    localStorage.setItem('classSection_list', JSON.stringify(newClassSections));
+    localStorage.setItem('class_list', JSON.stringify(classNums));
     closeModal();
+    window.location.reload();
 }
 
 //----------------------------------------------------------------------------------------------------
 
 // Loads the current courses for a user
 //Store the courses a student is taking in localStorage
-const class_dict = [];
-const storedCourses = JSON.parse(localStorage.getItem('class_list'));
-const class_list = storedCourses ? storedCourses : ['57-132', '67-443', '67-373'];
 
 function loadCurrentCourses(){
   //Grab the current course numbers enrolled in
-  var courseNums = [];
-  var eventContainer = document.getElementsByClassName("calendar-view")[0];
-  eventContainer.innerHTML = "";
-  for(let i = 0; i < currentCourses.length; i++){
-    courseNums.push(currentCourses[i].courseNum);
-  }
-  console.log(courseNums);
-  localStorage.setItem('class_list', JSON.stringify(courseNums));
- 
-  //Display the widgets
-  for(let i = 0; i < currentCourses.length; i++){
-    renderEvent(currentCourses[i], eventContainer);
-  }
-}
-
-//Renders the event with the appropriate variables
-function renderEvent(evt, eventContainer) {
   const colors = [
     "red",
     "green",
@@ -138,6 +129,30 @@ function renderEvent(evt, eventContainer) {
     "darkslategray",
     "mediumblue"
   ];
+  var courseNums = [];
+  var courseSections = [];
+  var eventContainer = document.getElementsByClassName("calendar-view")[0];
+  eventContainer.innerHTML = "";
+  for(let i = 0; i < currentCourses.length; i++){
+    courseNums.push(currentCourses[i].courseNum);
+  }
+  console.log(courseNums);
+  console.log(courseSections);
+  localStorage.setItem('class_list', JSON.stringify(courseNums));
+  localStorage.setItem('classSection_list', JSON.stringify(classSection_list));
+ 
+  //Display the widgets
+  for(let i = 0; i < currentCourses.length; i++){
+    var daysRegistered = grabWeekday(currentCourses[i].courseNum);
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    for(let d = 0; d < 2; d++){
+      renderEvent(currentCourses[i], eventContainer, daysRegistered[d], color);
+    }
+  }
+}
+
+//Renders the event with the appropriate variables
+function renderEvent(evt, eventContainer, day, color) {
   //Creates the element
   var oneEvent = document.createElement("div");
   var link = document.createElement("a");
@@ -145,9 +160,10 @@ function renderEvent(evt, eventContainer) {
   var eventStatus = document.createElement("div");
   var eventName = document.createElement("div");
   var eventTitle = document.createElement("div");
-  const color = Math.floor(Math.random() * colors.length);
+  var courseTimes = grabTimes(evt.courseNum);     
   eventName.innerHTML = `${evt.courseNum}`;
   eventTitle.innerHTML = `${evt.title}`;
+
 
   link.setAttribute('href', "mobile_detail.html");
   link.setAttribute("class", "event-name");
@@ -165,14 +181,14 @@ function renderEvent(evt, eventContainer) {
   /**
    * if two events have same start time
    */
-  oneEvent.style.background = colors[color];
+  oneEvent.style.background = color;
   oneEvent.style.width = evt.width + "%";
   oneEvent.style.left = evt.left + "%";
   oneEvent.style.zIndex = evt.zindex;
-  oneEvent.style.height = getHeight(evt.startTime, evt.endTime) + "px";
+  oneEvent.style.height = getHeight(courseTimes[0].startTime, courseTimes[0].endTime) + "px";
   // 100 / ((8-colPos))
-  oneEvent.style.gridColumnStart = getColumnPosition(evt.dayOfWeek);
-  oneEvent.style.gridRowStart = getRowPosition(evt.startTime);
+    oneEvent.style.gridColumnStart = getColumnPosition(day);
+    oneEvent.style.gridRowStart = getRowPosition(courseTimes[0].startTime);
 
   // add to event container
   eventContainer.appendChild(oneEvent);
@@ -212,10 +228,8 @@ function getColumnPosition(weekday) {
 }
 
 function getRowPosition(starttime) {
-  console.log(starttime);
   const h = +starttime.split(":")[0];
   const m = +starttime.split(":")[1];
-  console.log(h, m);
   const totalMinutes = Math.abs(8 - h) * 60 + m;
   const rowPos = totalMinutes /15 + 1;
 
@@ -234,14 +248,18 @@ function loadCourse(){
   var course = JSON.parse(localStorage.getItem('course'));
   courseDetails(course);
   populateCalendarView(course);
+  fillSections(course)
 }
 
+//Fill in the details of the courses and create the HTML elements
 function courseDetails(courseNum){
+  var classSections = JSON.parse(localStorage.getItem('classSection_list'));
+  var courseSection = classSections.find(e => e.courseNum === courseNum);
   var course = currentCourses.find(e => e.courseNum === courseNum);
   var head = course.courseNum + " " + course.title;
   var req = course.fulfilled;
   var prof = course.professor;
-  var section = course.section;
+  var section = courseSection.section;
   var units = course.units;
   var fce = course.fce;
   var desc = course.description;
@@ -274,10 +292,11 @@ function populateCalendarView(numSelected){
   //Formatting the data cells to display the courses
   var elementClassName = ["one", "two", "three"];
   for(let j = 0; j < classInfo.length; j++){
-    let c1 = classInfo[j].index[0].col;
-    let r1 = classInfo[j].index[0].row;
-    let c2 = classInfo[j].index[1].col;
-    let r2 = classInfo[j].index[1].row;
+    var tableIndex = sectionConvertToIndices(classInfo[j].courseNum);
+    let c1 = tableIndex[0].col;
+    let r1 = tableIndex[0].row;
+    let c2 = tableIndex[1].col;
+    let r2 = tableIndex[1].row;
     let num = classInfo[j].courseNum;
     let className = elementClassName[j];
     table.rows[r1].cells[c1].classList.add(num);
@@ -294,6 +313,119 @@ function populateCalendarView(numSelected){
 }
 
 //populates the section choices for a course on the detail page
-function fillSections(){
-  
+function fillSections(courseNum){
+  //Sets the body of the switching sections modal
+  var switchBody = document.getElementById("modalBodySwitch");
+  var course = allCourses.find(e => e.courseNum === courseNum);
+  var sections = course.sections;
+
+  let form = document.createElement('form');
+  form.setAttribute("name", "sections");
+  for(section of sections){
+    // BUILD THE RADIO BUTTON
+    let radio = document.createElement('input');
+    radio.setAttribute('type', 'radio');
+    radio.setAttribute('name', 'sections');
+    radio.setAttribute('value', section);
+    form.appendChild(radio);
+    
+    // BUILD THE LABEL
+    let label = document.createElement('label');
+    label.setAttribute('for', section);
+    label.textContent = section;
+    form.appendChild(label);
+        
+    // BUILD THE LINEBREAK
+    let linebreak = document.createElement('br');
+    form.appendChild(linebreak);
+  }
+
+  switchBody.appendChild(form);
+
+  //Fills in the necessary parameters for the switch button
+  var switchButton = document.getElementById("switchBTN");
+  switchButton.setAttribute("onclick", 'switchSection("'+course.courseNum+'")')
+
+  //Filling in the remove modal
+  var removeButton = document.getElementById("removeBTN");
+  removeButton.setAttribute("onclick", 'removeSection("'+course.courseNum+'")');
+  document.getElementById("removeCourse").innerHTML = "Are you sure you want to remove " + course.courseNum + " " + course.title + "?";
+}
+
+//----------------------------------------------------------------------------------------------------------------------------
+//converts the section stored in current courses to their appropriate times slots
+
+//returns the index dictionary needed for the calendar table
+function sectionConvertToIndices(courseNum){
+  var classSections = JSON.parse(localStorage.getItem('classSection_list'));
+  var indices = [];
+  var r = 0;
+  var c1 = 0;
+  var c2 = 0;
+  var course = classSections.find(e => e.courseNum === courseNum);
+  var sectionNum = course.section.substring(0,1);
+  var sectionLetter1 = course.section.substring(1,2);
+  var sectionLetter2 = course.section.substring(2);
+  for(let i = 0; i < sectionNums.length; i++){
+    if(sectionNums[i].num === sectionNum){
+      r = sectionNums[i].rowIndex;
+    }
+  }
+  for(let j = 0; j < sectionLetters.length; j++){
+    if(sectionLetters[j].num === sectionLetter1){
+      c1 = sectionLetters[j].colIndex;
+    }
+  }
+  for(let k = 0; k < sectionLetters.length; k++){
+    if(sectionLetters[k].num === sectionLetter2){
+      c2 = sectionLetters[k].colIndex;
+    }
+  }
+  var index1 = {"row": r, "col": c1};
+  var index2 = {"row": r, "col": c2};
+  indices.push(index1);
+  indices.push(index2);
+  return indices;
+}
+
+function grabWeekday(courseNum){
+  var classSections = JSON.parse(localStorage.getItem('classSection_list'));
+  var weekdays = [];
+  var course = classSections.find(e => e.courseNum === courseNum);
+  var sectionLetter1 = course.section.substring(1,2);
+  var sectionLetter2 = course.section.substring(2);
+  var firstDay = "";
+  var secondDay = "";
+  for(let j = 0; j < sectionLetters.length; j++){
+    if(sectionLetters[j].num === sectionLetter1){
+      firstDay = sectionLetters[j].dayOfWeek;
+    }
+  }
+  for(let k = 0; k < sectionLetters.length; k++){
+    if(sectionLetters[k].num === sectionLetter2){
+      secondDay = sectionLetters[k].dayOfWeek;
+    }
+  }
+  weekdays.push(firstDay);
+  weekdays.push(secondDay);
+  return weekdays;
+}
+
+function grabTimes(courseNum){
+  var classSections = JSON.parse(localStorage.getItem('classSection_list'));
+  var course = classSections.find(e => e.courseNum === courseNum);
+  var sectionNum = course.section.substring(0,1);
+  var times = [];
+  var startTime = "";
+  var endTime = "";
+  for(let i = 0; i < sectionNums.length; i++){
+    if(sectionNums[i].num === sectionNum){
+      startTime = sectionNums[i].startTime;
+      endTime = sectionNums[i].endTime;
+    }
+  }
+  var courseTime = {"startTime": startTime, "endTime": endTime};
+  times.push(courseTime);
+  console.log(times);
+  return times;
 }
